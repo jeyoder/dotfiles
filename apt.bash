a6702231863142c5e192f05e998f8f907ffd5c16
James Yoder <james@jyoder.com>
2020-01-16 10:17:53 -0600

    more packages

--- apt.bash
+++ apt.bash
@@ -4,4 +4,4 @@ sudo apt update
 
 sudo apt upgrade
 
-sudo apt install fish tmux pv vim xz-utils unzip p7zip udhcpd strace rsync gcc g++ gdb openssh-server git htop cmake python python3 python-dev python3-dev build-essential traceroute        
+sudo apt install fish tmux pv vim xz-utils unzip p7zip udhcpd strace rsync gcc g++ gdb openssh-server git htop cmake python python3 python-dev python3-dev build-essential traceroute smartmontools lm-sensors       
