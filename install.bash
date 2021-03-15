e493562450ca1936832ab927a320b5ffdbea6ef6
James Yoder <james@jyoder.com>
2021-03-15 14:39:19 -0500

    things

--- install.bash
+++ install.bash
@@ -10,5 +10,7 @@ set -o xtrace
 
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/vim ~/.vim
+ln -s $DIR/tmux.conf ~/.tmux.conf
+ln -s $DIR/bashrc ~/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
